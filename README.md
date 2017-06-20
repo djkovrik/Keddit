ebaa515ac4244b2e20add0ba7578fe12d0a6b858
DJ_Kovrik <djkovrik@gmail.com>
2017-06-20 15:01:31 +0300

    Part 2

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Sample app based on Kotlin related article series:
 * [Intro: Learn Kotlin while developing an Android App](https://android.jlelse.eu/learn-kotlin-while-developing-an-android-app-introduction-567e21ff9664)
 * [Part 1: Configuring Android Studio with Kotlin](https://android.jlelse.eu/learn-kotlin-while-developing-an-android-app-part-1-e0f51fc1a8b3)
+* [Part 2: Kotlin Syntax, Null Safety and more in Android](https://android.jlelse.eu/learn-kotlin-while-developing-an-android-app-part-2-e53317ffcbe9)
--- app/src/main/java/com/sedsoftware/keddit/MainActivity.kt
+++ app/src/main/java/com/sedsoftware/keddit/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.sedsoftware.keddit
 
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import android.view.Menu
-import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,31 +13,34 @@ class MainActivity : AppCompatActivity() {
     setContentView(R.layout.activity_main)
     val toolbar = findViewById(R.id.toolbar) as Toolbar
     setSupportActionBar(toolbar)
+  }
 
-    val fab = findViewById(R.id.fab) as FloatingActionButton
-    fab.setOnClickListener { view ->
-      Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-          .setAction("Action", null).show()
+  override fun onBackPressed() {
+    val manager = supportFragmentManager
+    if (manager.backStackEntryCount > 1) {
+      manager.popBackStack()
+    } else {
+      finish()
     }
   }
 
-  override fun onCreateOptionsMenu(menu: Menu): Boolean {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    menuInflater.inflate(R.menu.menu_main, menu)
-    return true
+  fun changeFragment(fragment: Fragment, cleanStack: Boolean = false) {
+    val transaction = supportFragmentManager.beginTransaction()
+    if (cleanStack) {
+      cleanStack()
+    }
+    transaction.setCustomAnimations(R.anim.abc_fade_in, R.anim.abc_fade_out, R.anim.abc_popup_enter,
+        R.anim.abc_popup_exit)
+    transaction.replace(R.id.activity_base_content, fragment)
+    transaction.addToBackStack(null)
+    transaction.commit()
   }
 
-  override fun onOptionsItemSelected(item: MenuItem): Boolean {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    val id = item.itemId
-
-
-    if (id == R.id.action_settings) {
-      return true
+  fun cleanStack() {
+    val manager = supportFragmentManager;
+    if (manager.backStackEntryCount > 0) {
+      val first = manager.getBackStackEntryAt(0);
+      manager.popBackStack(first.id, FragmentManager.POP_BACK_STACK_INCLUSIVE);
     }
-
-    return super.onOptionsItemSelected(item)
   }
 }
