cb2934a6fbfef5b005c36e709889bfdd82963d95
DJ_Kovrik <djkovrik@gmail.com>
2017-06-21 21:33:50 +0300

    Part 4: model classes

---
+++ app/src/main/java/com/sedsoftware/keddit/commons/Models.kt
@@ -0,0 +1,62 @@
+package com.sedsoftware.keddit.commons
+
+import android.os.Parcel
+import android.os.Parcelable
+import com.sedsoftware.keddit.commons.adapter.AdapterConstants
+import com.sedsoftware.keddit.commons.adapter.ViewType
+
+data class RedditNews(
+    val after: String,
+    val before: String,
+    val news: List<RedditNewsItem>) : Parcelable {
+  companion object {
+    @JvmField val CREATOR: Parcelable.Creator<RedditNews> = object : Parcelable.Creator<RedditNews> {
+      override fun createFromParcel(source: Parcel): RedditNews = RedditNews(source)
+      override fun newArray(size: Int): Array<RedditNews?> = arrayOfNulls(size)
+    }
+  }
+
+  constructor(source: Parcel) : this(source.readString(), source.readString(),
+      source.createTypedArrayList(RedditNewsItem.CREATOR))
+
+  override fun describeContents() = 0
+
+  override fun writeToParcel(dest: Parcel?, flags: Int) {
+    dest?.writeString(after)
+    dest?.writeString(before)
+    dest?.writeTypedList(news)
+  }
+}
+
+data class RedditNewsItem(
+    val author: String,
+    val title: String,
+    val numComments: Int,
+    val created: Long,
+    val thumbnail: String,
+    val url: String?
+) : ViewType, Parcelable {
+
+  override fun getViewType() = AdapterConstants.NEWS
+
+  companion object {
+    @JvmField val CREATOR: Parcelable.Creator<RedditNewsItem> = object : Parcelable.Creator<RedditNewsItem> {
+      override fun createFromParcel(source: Parcel): RedditNewsItem = RedditNewsItem(source)
+      override fun newArray(size: Int): Array<RedditNewsItem?> = arrayOfNulls(size)
+    }
+  }
+
+  constructor(source: Parcel) : this(source.readString(), source.readString(), source.readInt(),
+      source.readLong(), source.readString(), source.readString())
+
+  override fun describeContents() = 0
+
+  override fun writeToParcel(dest: Parcel?, flags: Int) {
+    dest?.writeString(author)
+    dest?.writeString(title)
+    dest?.writeInt(numComments)
+    dest?.writeLong(created)
+    dest?.writeString(thumbnail)
+    dest?.writeString(url)
+  }
+}
